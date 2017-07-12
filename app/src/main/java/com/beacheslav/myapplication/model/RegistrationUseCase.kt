1a0e6d17725c2aec5c93af3ace777e2fb6bc739a
Пользователь <beacheslav@mail.ru>
2017-07-13 01:53:28 +0500

    SQLite request

---
+++ app/src/main/java/com/beacheslav/myapplication/model/RegistrationUseCase.kt
@@ -0,0 +1,5 @@
+package com.beacheslav.myapplication.model.entity
+
+/**
+ * Created by Пользователь on 12.07.2017.
+ */
---
+++ app/src/main/java/com/beacheslav/myapplication/model/okHttpKlient.kt
@@ -0,0 +1,5 @@
+package com.beacheslav.myapplication.model
+
+/**
+ * Created by Пользователь on 12.07.2017.
+ */
